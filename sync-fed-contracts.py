1caa4cfac7e363ac5438c8f094bc4e988cee779c
Mike Stucka <stucka@whitedoggies.com>
2025-02-24 20:36:04 -0500

    Split out some code. Handle files already downloaded

--- sync-fed-contracts.py
+++ sync-fed-contracts.py
@@ -23,14 +23,21 @@ datadir = "data/"
 os.makedirs(datadir, exist_ok=True)
 
 
-async def fetch_a_date(localdate):
+def screen_files(localdate):
     needfiles = False
     filedate = localdate.strftime("%Y-%m-%d")
     for reason in reasons:
         filename = f"{datadir}/contracts-{filedate}_{reason}.json"
         if not os.path.exists(filename):
             needfiles = True
-    if needfiles:
+    return(needfiles)
+
+
+async def fetch_a_date(localdate):
+    needfiles = screen_files(localdate)
+    if not needfiles:
+        return None
+    else:
         requests = {}
         requestdate = localdate.strftime("%Y/%m/%d")
         async with asyncio.TaskGroup() as tg:
@@ -50,18 +57,20 @@ if __name__ == "__main__":
     today = datetime.datetime.now()
     start = datetime.datetime(2025, 1, 20)
     days_to_find = (today - start).days
+    
+    # Build out a progress bar, because the code runs take time.
     with tqdm(total=days_to_find, desc="1900-00-00") as pbar:
-        for dateincrement in range(0, days_to_find):
-            localdate = start + datetime.timedelta(days=dateincrement)
-            filedate = localdate.strftime("%Y-%m-%d")
-            pbar.set_description(filedate)
+        for dateincrement in range(0, days_to_find):    # Current date/today has no data
             targetdate = start + datetime.timedelta(days=dateincrement)
+            filedate = targetdate.strftime("%Y-%m-%d")
+            pbar.set_description(filedate)
             data = asyncio.run(fetch_a_date(targetdate))
-            for reason in reasons:
-                filename = f"{datadir}/contracts-{filedate}_{reason}.json"
-                localdata = data[reason]
-                with open(filename, "w", encoding="utf-8") as outfile:
-                    outfile.write(json.dumps(localdata, indent=4 * " "))
+            if data:
+                for reason in reasons:
+                    filename = f"{datadir}/contracts-{filedate}_{reason}.json"
+                    localdata = data[reason]
+                    with open(filename, "w", encoding="utf-8") as outfile:
+                        outfile.write(json.dumps(localdata, indent=4 * " "))
             pbar.update(1)
 
 ignore = """
